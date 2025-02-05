666bce68006dec0e3fe2f156c1bbca4fa88b88c9
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2025-02-05 20:53:10 +0100

    Redundant code removal

--- Program.cs
+++ Program.cs
@@ -21,39 +21,13 @@ var msg2 = CustomProtocolMessage.FromBytes(msg.ToByteArray());
 CustomUdpClient udpClient = new CustomUdpClient ();
 Console.Write("Enter address for listening:");
 string address = Console.ReadLine();
-if(address == "t1")
-{
-    udpClient.Start("127.0.0.1", 5050,8080);
-}else if(address == "t2"){
-    udpClient.Start("127.0.0.1", 5656,6565);
-    await udpClient.Connect(5050, "127.0.0.1");
-}
-else if(address == "t24"){
-    udpClient.Start("127.0.0.1", 5656,6565);
-    await udpClient.Connect(5050, "127.0.0.1");
-   // await udpClient.SendFile()
-}
-else if(address == "t3"){
-    udpClient.Start("192.168.1.40", 10080,10080);
 
-}else if(address == "t4")
-{
-    udpClient.Start("192.168.1.68", 5050,8080);
-    
-}else
-{
-    Console.Write("Enter port for listening:");
-    ushort listeningPort = Convert.ToUInt16(Console.ReadLine());
-    Console.Write("Enter port for sending:");
-    ushort sendingPort = Convert.ToUInt16(Console.ReadLine());
-  
-    udpClient.Start(address, listeningPort, sendingPort);
-}
+Console.Write("Enter port for listening:");
+ushort listeningPort = Convert.ToUInt16(Console.ReadLine());
+Console.Write("Enter port for sending:");
+ushort sendingPort = Convert.ToUInt16(Console.ReadLine());
 
-//udpClient.Start("127.0.0.1", 5050, 9911);
-//udpClient.Start("127.0.0.1", listeningPort, sendingPort);
-
-
+udpClient.Start(address, listeningPort, sendingPort);
 
 while(true)
 {
